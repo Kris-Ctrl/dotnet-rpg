4f38cec5f8facc849455e27e730caa1bfef5ca43
Kris <tfm6325@autuni.ac.nz>
2022-11-08 15:24:36 +1300

    Login and register with Dtos

---
+++ Migrations/20221103004653_UserModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_rpg.Migrations
+{
+    public partial class UserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_characters",
+                table: "characters");
+
+            migrationBuilder.RenameTable(
+                name: "characters",
+                newName: "Characters");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Characters",
+                table: "Characters",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Characters",
+                table: "Characters");
+
+            migrationBuilder.RenameTable(
+                name: "Characters",
+                newName: "characters");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_characters",
+                table: "characters",
+                column: "Id");
+        }
+    }
+}
